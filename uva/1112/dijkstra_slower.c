413ab7cd07566fd99502f557df59f6ac486f3345
Pedro-V <mingas62@hotmail.com>
2024-07-16 15:20:40 -0300

    Dijkstra on a reversed graph

--- uva/1112/dijkstra_slower.c
+++ uva/1112/dijkstra_slower.c
@@ -11,16 +11,12 @@ typedef struct edge {
 } edge;
 
 /*
- * This is a Dijkstra implementation similar to `dijkstra_slower.c`,
- * but this reverses the edges in the input graph, which allows one to call
- * Djikstra's algorithm on the exit cell and and the just run over the distances
- * array to check for the solution.
+ * This is a Dijkstra implementation with a simple array search.
+ * That's okay because n <= 100.
 */
 
-
-/* Populates the single-source shortest paths array (min_times) for the exit cell. */
-void find_min_times(edge *adj_list[], int min_times[], int num_cells, int exit_cell) {
-  int visited[SIZE + 1] = { 0 };
+int min_time(edge *adj_list[], int num_cells, int cell, int exit_cell) {
+  int min_times[SIZE + 1], visited[SIZE + 1] = { 0 };
   int min_time_idx, i, j, min_time, found;
   edge *e;
 
@@ -28,7 +24,7 @@ void find_min_times(edge *adj_list[], int min_times[], int num_cells, int exit_c
     visited[i] = 0;
     min_times[i] = -1;
   }
-  min_times[exit_cell] = 0;
+  min_times[cell] = 0;
 
   for (i = 1; i <= num_cells; i++) {
     /* Find the next vertex */
@@ -59,14 +55,15 @@ void find_min_times(edge *adj_list[], int min_times[], int num_cells, int exit_c
 
     visited[min_time_idx] = 1;
   }
+
+  return min_times[exit_cell];
 }
 
 int main() {
   static edge *adj_list[SIZE + 1] = {NULL};
-  int min_times[SIZE + 1];
   edge *e;
   int num_cases, num_cells, num_passages, limit, exit_cell;
-  int i, case_num, from, to, cost, result;
+  int i, case_num, from, to, cost, result, mice_time;
 
   scanf("%d", &num_cases);
   for (case_num = 1; case_num <= num_cases; case_num++) {
@@ -78,19 +75,19 @@ int main() {
     for (i = 0; i < num_passages; i++) {
       scanf("%d %d %d", &from, &to, &cost);
 
-      /* Reverses the graph. Compare with `dijkstra_slower.c` */
       e = malloc(sizeof(edge));
-      e->to = from;
+      e->to = to;
       e->cost = cost;
-      e->next = adj_list[to];
-      adj_list[to] = e;
+      e->next = adj_list[from];
+      adj_list[from] = e;
     }
 
     result = 0;
-    find_min_times(adj_list, min_times, num_cells, exit_cell);
-    for (i = 1; i <= num_cells; i++)
-      if (min_times[i] >= 0 && min_times[i] <= limit)
+    for (i = 1; i <= num_cells; i++) {
+      mice_time = min_time(adj_list, num_cells, i, exit_cell);
+      if (mice_time >= 0 && mice_time <= limit)
         result++;
+    }
 
     printf("%d\n", result);
     if (case_num < num_cases)
@@ -99,4 +96,3 @@ int main() {
 
   return 0;
 }
-
